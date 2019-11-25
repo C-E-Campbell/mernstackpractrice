68ef774a5e5db881a176a31fa37f89d9adcdf338
charles <codelyfewithcharlie@gmail.com>
2019-11-24 18:49:04 -0700

    new components

---
+++ src/user/components/UserItem.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./UsersItem.css";
+
+export default function UserItem(props) {
+  return <div>userItem</div>;
+}
---
+++ src/user/components/UsersList.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./UserList.css";
+
+export default function UsersList(props) {
+  return <div>User List</div>;
+}
