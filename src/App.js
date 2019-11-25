cc6220021795f8913e233f6bbeb2d8e78a65b294
charles <codelyfewithcharlie@gmail.com>
2019-11-24 22:05:50 -0700

    updates

--- src/App.js
+++ src/App.js
@@ -7,9 +7,11 @@ import {
 } from "react-router-dom";
 import Users from "./user/pages/Users";
 import Places from "./places/pages/NewPlace";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
 function App() {
   return (
     <Router>
+      <MainNavigation />
       <Switch>
         <Route path="/" exact component={Users} />
         <Route path="/places/new" exact component={Places} />
